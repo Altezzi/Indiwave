2653adac0b1a861384c2d463230ac847872f60c5
Tyler Hurst <107834387+Altezzi@users.noreply.github.com>
2025-09-17 01:14:12 -0400

    Update route.ts

--- app/api/comics/route.ts
+++ app/api/comics/route.ts
@@ -2,5 +2,5 @@ import { NextResponse } from "next/server";
 import data from "../../../data/comics.json";
 
 export async function GET() {
-  return NextResponse.json({ comics: data.comics.map(({ pages, ...rest }) => rest) });
+  return NextResponse.json({ comics: data.comics });
 }
